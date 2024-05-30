e9320bd85752e6a6495948d3482de7f74af5931c
tianheg <i@tianheg.co>
2024-05-30 10:36:54 +0800

    feat: basic design is DONE!

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
